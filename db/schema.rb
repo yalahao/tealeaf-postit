ce96b22ce68a5a641657b6f4fce35ef05ced718f
Hal Chen <yalahao@gmail.com>
2015-05-03 20:29:55 +0200

    Set up all models

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503152528) do
+ActiveRecord::Schema.define(version: 20150503182816) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "post_id"
   end
 
   create_table "posts", force: true do |t|
