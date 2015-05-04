1a7d2b6d9a4b638611afed166fb6ed551f2a40cc
Hal Chen <yalahao@gmail.com>
2015-05-04 07:05:19 +0200

    Added all models and basic posts views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index
 
+  def index
+    @posts = Post.all
   end
-end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503182816) do
+ActiveRecord::Schema.define(version: 20150504013238) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150503182816) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
