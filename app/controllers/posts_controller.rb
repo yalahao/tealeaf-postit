9fabbd4fcbbe5ca6de98de616f6f2e90b448aa08
Hal Chen <yalahao@gmail.com>
2015-05-05 12:01:12 +0200

    Set up post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,44 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.create(post_params)
+    @post.creator = User.first
+    if @post.save
+      flash[:notice] = "Post created."
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.update(Post.find(params[:id]), post_params)
+    if @post.save
+      flash[:notice] = "Post updated."
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :description, :url)
+    end
 
 end 
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :url, presence: true, uniqueness: true
+  validates :creator, presence: true
 end
