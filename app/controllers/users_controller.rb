a0865b9321cbcba193c4ac325401930c2443858b
Hal Chen <yalahao@gmail.com>
2015-05-15 13:47:23 +0200

    Added users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
+  before_action :get_user, only: [:show, :update]
+
+  def new 
+    @user = User.new
+  end 
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "You are registered."
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+
+    def get_user
+      @user = User.find(params[:id])
+    end
 
 end
