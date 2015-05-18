97262540583f94f51c020db4c24e70f2390728fd
Hal Chen <yalahao@gmail.com>
2015-05-18 20:51:58 +0200

    Added conditions to _vote_box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
     datetime.strftime("on %B %-d, %Y")
   end
 
+  def vote_status(user, obj)
+    current_user_vote = obj.votes.find_by(user_id: user.id)
+    if current_user_vote
+      current_user_vote.vote
+    else
+      nil
+    end
+  end
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,4 @@ class Vote < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :voteable, polymorphic: true
 
-  def user_vote_status(user, obj)
-
-  end
-
 end
