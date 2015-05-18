ea30e547518e858e14ad2eb6f243ca8b40864cc6
Hal Chen <yalahao@gmail.com>
2015-05-18 04:54:38 +0200

    Added votes table

---
+++ db/migrate/20150518025138_create_votes_table.rb
@@ -0,0 +1,10 @@
+class CreateVotesTable < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.boolean :vote
+      t.integer :user_id
+      t.string :voteable_type
+      t.integer :voteable_id
+    end
+  end
+end
