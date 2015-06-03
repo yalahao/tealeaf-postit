38b181f1a50a6333fb049cadf9dbc6017e5f8672
Hal Chen <yalahao@gmail.com>
2015-06-03 07:21:17 +0200

    Disabled post editing for non-creators

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   before_action :get_post, only: [:show, :edit, :update, :vote, :vote_count]
   before_action :require_user, except: [:index, :show]
+  before_action :require_current_user, only: [:edit]
 
   def new
     @post = Post.new
@@ -45,9 +46,7 @@ class PostsController < ApplicationController
     else
       flash[:error] = "You can only vote once per item."
     end
-
     redirect_to :back
-
   end
 
 
@@ -59,6 +58,13 @@ class PostsController < ApplicationController
 
     def get_post
       @post = Post.find(params[:id])
+    end
+
+    def require_current_user
+      if @post.creator != current_user
+        flash[:error] = "You are not allowed to do that."
+        redirect_to :back
+      end
     end
 
 end 
