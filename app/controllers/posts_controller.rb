825f99521493169cf4a0ccc2655b9f1697e897ba
Hal Chen <yalahao@gmail.com>
2015-06-03 21:16:18 +0200

    Ajaxifying the vote count

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,12 +41,17 @@ class PostsController < ApplicationController
 
   def vote
     @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
-    if @vote.valid?
-      flash[:notice] = "Your vote was counted."
-    else
-      flash[:error] = "You can only vote once per item."
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted."
+        else
+          flash[:error] = "You can only vote once per item."
+        end
+        redirect_to :back
+      end 
+      format.js
     end
-    redirect_to :back
   end
 
 
